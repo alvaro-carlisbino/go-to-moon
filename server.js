e22809d642cf1cedc155699570d97a197e9578f0
Bryzzen <xdbrgames10@gmail.com>
2022-05-14 23:09:28 -0300

    Beta

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const ejs = require("ejs")
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => res.render("index"))
+app.get("/indo", (req, res) => res.render("indo"))
+app.get("/about", (req, res) => res.render("sobre"))
+app.use((req, res) => res.render("index"))
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 80, console.log("Site iniciado com sucesso <3"))
