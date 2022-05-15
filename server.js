d83277a9bfff72e8089731901bc59341d115c746
Glitch (go-to-moon) <none>
2022-05-15 00:16:10 +0000

    🎀💧 Updated with Glitch

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const app = express();
-const ejs = require("ejs");
+const express = require("express")
+const app = express()
+const ejs = require("ejs")
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
-app.get("/", (res, req) => req.render("index"));
+app.get("/", (res, req) => req.render("index")) // a
 
-app.listen(process.env.PORT || 80, console.log("Site iniciado com sucesso <3"));
+app.listen(process.env.PORT || 80, console.log("Site iniciado com sucesso <3"))
